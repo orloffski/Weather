5e8f563933191e2d56c5f603d461dc80f88cf7d8
Evgen Orlovski <orloffski@gmail.com>
2023-01-29 02:48:03 +0300

    add: add log.service.js for console log

---
+++ services/log.service.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+export const printError = (error) => {
+	console.log(chalk.bgRed.bold(' ERROR ') + ' ' + error);
+}
+
+export const printSuccess = (success) => {
+	console.log(chalk.bgGreen.bold(' SUCCESS ') + ' ' + success);
+}
+
+export const printHelp = () => {
+	console.log(
+		dedent`${chalk.bgBlackBright.bold(' HELP ')}
+		Without parameters - return weather
+		-s [CITY] for setup city
+		-h for help
+		-t [API_KEY] for token save
+		`);
+}
