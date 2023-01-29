7d83e5dce7bc824753df7823f10a428ad951c9e7
Evgen Orlovski <orloffski@gmail.com>
2023-01-29 03:33:45 +0300

    feat: add logic save token

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,9 +1,36 @@
 import { homedir } from 'os';
 import { join } from 'path';
+import { readFile, stat, writeFile } from 'fs/promises';
 
 const FILE_NAME = 'weather-data.json';
 const filePath = join(homedir(), FILE_NAME);
 
-export const saveKeyValue = (objKeyValue) => {
-	console.log(filePath);
+export const saveKeyValue = async (objKeyValue) => {
+	let data = {};
+
+	if(await isExist(filePath)){
+		const file = await readFile(filePath);
+		data = JSON.parse(file);
+	}
+
+	data[objKeyValue.key] = objKeyValue.value;
+
+	await writeFile(filePath, JSON.stringify(data));
+}
+
+export const getKey = async (key) => {
+	if(await isExist(filePath)){
+		const file = await readFile(filePath);
+		const data = JSON.parse(file);
+
+		return data[key];
+	}
+
+	return undefined;
+}
+
+const isExist = async (path) => {
+	stat(path)
+		.then(() => true)
+		.catch(() => false);
 }
--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
 import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try{
+		await saveKeyValue({key: 'token', value: token});
+		printSuccess('Token saved');
+	} catch (e){
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
@@ -16,7 +25,7 @@ const initCLI = () => {
 	}
 
 	if(args.t){
-		saveKeyValue({key: 'token', value: args.t});
+		saveToken(args.t)
 	}
 };
 
